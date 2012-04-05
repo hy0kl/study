4f8d4ffc796af82a225ea9cf9f56c59b7cf54ca5
hy0kl <hy0kle@gmail.com>
2012-04-05 11:35:57 +0800

    Add code to test point for function.

--- c-study/point.c
+++ c-study/point.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int test(int *fd)
 {
@@ -12,9 +13,18 @@ int test(int *fd)
 int main(int argc, char *argv[])
 {
     int t = 128;
+    int ret = -9;
+
     test(&t);
 
     printf("Now, t is: %d in %s.\n", t, __FUNCTION__);
+
+    int (*fun)(const char *s1, const char *s2, size_t n) = NULL;
+    fun = strncmp;
+
+    printf("Before cmp, ret is: %d\n", ret);
+    ret = fun("hello", "Test", 4);
+    printf("After cmp, ret is: %d\n", ret);
 
     return 0;
 }
