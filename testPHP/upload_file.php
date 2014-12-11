39b3ac87ceb28c1abc9eb2c9dcae91ad9e7a5ebc
Jerry Yang <hy0kle@gmail.com>
2014-12-11 22:36:01 +0800

    常常要回来查,索性收录进来,运行一下就明白了.

---
+++ testPHP/upload_file.php
@@ -0,0 +1,23 @@
+<html>   
+<body>   
+<form action="?" id="up_file" encType="multipart/form-data" method="post">   
+    <input type="file" id="upload_file" name="upload_file">   
+    <input type="submit" value="上传文件"><span id="msg"></span>   
+    <br>   
+    <font color="red">支持JPG,JPEG,GIF,BMP,SWF,RMVB,RM,AVI文件的上传</font>                 
+</form>   
+</body>   
+</html>
+<?php
+/**
+ * @describe:
+ * @author: Jerry Yang(hy0kle@gmail.com)
+ * */
+if (isset($_FILES) && count($_FILES))
+{
+    echo '<pre>';
+    print_r($_FILES);
+    echo '</pre>';
+}
+/* vi:set ts=4 sw=4 et fdm=marker: */
+
