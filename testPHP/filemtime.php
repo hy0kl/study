1701977eee7c37efe5bfe54b34a8878eac9c63d8
Jerry Yang <hy0kle@gmail.com>
2014-12-31 14:37:22 +0800

    加入一些测试结果说明.

--- testPHP/filemtime.php
+++ testPHP/filemtime.php
@@ -3,6 +3,23 @@
  * @describe:
  * @author: Jerry Yang(hy0kle@gmail.com)
  * */
+/**
+http://wlog.cn/performance/assets-version.html
+
+function AutoVersion( $file ) {
+    if( file_exists($_SERVER['DOCUMENT_ROOT'].$file) ) {
+        $ver = filemtime($_SERVER['DOCUMENT_ROOT'] . $file);
+    } else {
+        $ver = 1;
+    }
+
+    return $file .'v=' .$ver;
+}
+
+<link rel="stylesheet" href="<?=AutoVersion('assets/css/style.css')?>" type="text/css" />
+
+通过测试发现,这种方法在水平多台机器部署时,文件最后修改时间可能各个服务器不一致
+ * */
 
 $self = './filemtime.php';
 
