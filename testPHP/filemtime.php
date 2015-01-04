749ffd1e9e7842915f71253b82abd52b7d2c7a54
Jerry Yang <hy0kle@gmail.com>
2015-01-04 11:13:23 +0800

    some note for 自动生成静态版本号

--- testPHP/filemtime.php
+++ testPHP/filemtime.php
@@ -19,13 +19,17 @@ function AutoVersion( $file ) {
 <link rel="stylesheet" href="<?=AutoVersion('assets/css/style.css')?>" type="text/css" />
 
 通过测试发现,这种方法在水平多台机器部署时,文件最后修改时间可能各个服务器不一致
+用 md5_file 替代最后修改时间更为靠谱
  * */
 
 $self = './filemtime.php';
 
 echo filemtime($self) . PHP_EOL;
 
+$start = microtime(true);
 echo md5_file($self) . PHP_EOL;
+$end = microtime(true);
+echo 'md5_file() exec time: ' . ($end - $start) .  PHP_EOL;
 
 /* vi:set ts=4 sw=4 et fdm=marker: */
 
