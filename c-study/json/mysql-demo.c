bbf82853efc793134d7f523b9ebd06959eb8928d
Jerry Yang <hy0kle@gmail.com>
2015-06-10 19:49:51 +0800

    加入输出字段名的代码.

--- c-study/json/mysql-demo.c
+++ c-study/json/mysql-demo.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
     MYSQL *conn_ptr;
     MYSQL_RES *res_ptr;
     MYSQL_ROW sqlrow;
-    MYSQL_FIELD *fd;
+    MYSQL_FIELD *field;
     int res, i, j;
 
     conn_ptr = mysql_init(NULL);
@@ -46,22 +46,32 @@ int main(int argc, char *argv[])
     }
 
     conn_ptr = mysql_real_connect(conn_ptr, "127.0.0.1", "dev", "dev", "oauth2", 3306, NULL, 0);
+
     if (conn_ptr) {
-        res = mysql_query(conn_ptr, "SELECT * FROM oauth_access_tokens"); //查询语句
+        res = mysql_query(conn_ptr, "SELECT access_token, client_id, user_id, expires FROM oauth_access_tokens"); //查询语句
         if (res) {
-            printf("SELECT error:%s\n",mysql_error(conn_ptr));
+            printf("SELECT error:%s\n", mysql_error(conn_ptr));
         } else {
             res_ptr = mysql_store_result(conn_ptr);             //取出结果集
             if(res_ptr) {
-                printf("%lu Rows\n",(unsigned long)mysql_num_rows(res_ptr));
+                printf("---%lu Rows---\n", (unsigned long)mysql_num_rows(res_ptr));
+
+                /** 组织字段 */
+                while((field = mysql_fetch_field(res_ptr))) {
+                    printf("%s\t", field->name);
+                }
+                printf("\n");
+
+                /** 输出记录 */
                 j = mysql_num_fields(res_ptr);
                 while((sqlrow = mysql_fetch_row(res_ptr)))  {   //依次取出记录
                     for(i = 0; i < j; i++)
                         printf("%s\t", sqlrow[i]);              //输出
                     printf("\n");
                 }
+
                 if (mysql_errno(conn_ptr)) {
-                    fprintf(stderr,"Retrive error:s\n",mysql_error(conn_ptr));
+                    fprintf(stderr, "Retrive error:%s\n", mysql_error(conn_ptr));
                 }
             }
             mysql_free_result(res_ptr);
@@ -69,6 +79,7 @@ int main(int argc, char *argv[])
     } else {
         printf("Connection failed\n");
     }
+
     mysql_close(conn_ptr);
 
     return EXIT_SUCCESS;
