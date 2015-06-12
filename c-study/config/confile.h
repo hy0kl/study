0be71ee0535551193ed9efe8f9cd7a9d00af6238
Jerry Yang <hy0kle@gmail.com>
2015-06-12 12:06:19 +0800

    头文件整理.

--- c-study/config/confile.h
+++ c-study/config/confile.h
@@ -12,25 +12,27 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-//#include <unistd.h>
+#include <unistd.h>
 
-typedef struct _CONFIG_BTREE_NODE{
-	char*	key;
-	void*	data;
-	struct _CONFIG_BTREE_NODE* left;
-	struct _CONFIG_BTREE_NODE* right;
-	char mem[2];
-}CONFIG_BTREE_NODE;
+typedef struct _CONFIG_BTREE_NODE
+{
+    char    *key;
+    void    *data;
+    struct _CONFIG_BTREE_NODE* left;
+    struct _CONFIG_BTREE_NODE* right;
+    char   mem[2];
+} CONFIG_BTREE_NODE;
 
-typedef struct _CONFIG_BTREE{
-	int numNodes;
-	CONFIG_BTREE_NODE* root;
-}CONFIG_BTREE;
+typedef struct _CONFIG_BTREE
+{
+    int                 numNodes;
+    CONFIG_BTREE_NODE  *root;
+} CONFIG_BTREE;
 
 typedef CONFIG_BTREE INI_CONFIG;
 
-typedef int (*CONFIG_BTREE_TRAVERSE_CB)(CONFIG_BTREE_NODE* node);
-typedef int (*CONFIG_BTREE_SAVE_TRAVERSE_CB)(FILE* fp,CONFIG_BTREE_NODE* node);
+typedef int (*CONFIG_BTREE_TRAVERSE_CB)(CONFIG_BTREE_NODE *node);
+typedef int (*CONFIG_BTREE_SAVE_TRAVERSE_CB)(FILE *fp, CONFIG_BTREE_NODE *node);
 
 /**
  * ini内容解析函数,从字符串解析配置
@@ -39,7 +41,8 @@ typedef int (*CONFIG_BTREE_SAVE_TRAVERSE_CB)(FILE* fp,CONFIG_BTREE_NODE* node);
  * @param isGBK 如果ini文件使用GBK字符集,设置成1,否则设置成0
  * @return 成功返回INI_CONFIG指针,失败返回null
  */
-INI_CONFIG* ini_config_create_from_string(char* str,int slen,int isGBK);
+INI_CONFIG *
+ini_config_create_from_string(char *str, int slen, int isGBK);
 
 /**
  * ini内容解析函数,从文件解析配置
@@ -47,14 +50,16 @@ INI_CONFIG* ini_config_create_from_string(char* str,int slen,int isGBK);
  * @param isGBK 如果ini文件使用GBK字符集,设置成1,否则设置成0
  * @return 成功返回INI_CONFIG指针,失败返回null
  */
-INI_CONFIG* ini_config_create_from_file(const char* filename,int isGBK);
+INI_CONFIG *
+ini_config_create_from_file(const char *filename, int isGBK);
 
 /**
  * 配置释放函数,释放所占用的内存及数据结构
  * @param config 配置对象指针
  * @return 成功返回１,失败返回０
  */
-void ini_config_destroy(INI_CONFIG* config);
+void
+ini_config_destroy(INI_CONFIG *config);
 /**
  * 获取配置整数值
  * @param config 配置对象指针
@@ -63,7 +68,9 @@ void ini_config_destroy(INI_CONFIG* config);
  * @param default_int　默认值
  * @return 如果配置中有此键对应的值,返回该值,否则返回参数指定的默认值　
  */
-int ini_config_get_int(INI_CONFIG* config,const char* section,const char* key,int default_int);
+int
+ini_config_get_int(INI_CONFIG *config, const char *section, const char *key, int default_int);
+
 /**
  * 获取配置字符串值
  * @param config 配置对象指针
@@ -72,7 +79,8 @@ int ini_config_get_int(INI_CONFIG* config,const char* section,const char* key,in
  * @param default_string　默认值
  * @return 如果配置中有此键对应的值,返回该值,否则返回参数指定的默认值　
  */
-char* ini_config_get_string(INI_CONFIG* config,const char* section,const char* key,char* default_string);
+char *
+ini_config_get_string(INI_CONFIG *config, const char *section, const char *key, char *default_string);
 /**
  * 设置变量
  * @param config 配置对象指针
@@ -83,7 +91,8 @@ char* ini_config_get_string(INI_CONFIG* config,const char* section,const char* k
  * @param value_len　值长度
  * @return 成功为１,失败为０
  */
-int ini_config_set_string(INI_CONFIG* config,const char* section,const char* key,int key_len,const char* value,int value_len);
+int
+ini_config_set_string(INI_CONFIG *config, const char *section, const char *key, int key_len, const char *value, int value_len);
 /**
  * 设置变量
  * @param config 配置对象指针
@@ -93,20 +102,23 @@ int ini_config_set_string(INI_CONFIG* config,const char* section,const char* key
  * @param value　整数值
  * @return 成功为１,失败为０
  */
-int ini_config_set_int(INI_CONFIG* config,const char* section,const char* key,int key_len,int value);
+int
+ini_config_set_int(INI_CONFIG *config, const char *section, const char *key, int key_len, int value);
 /**
  * 保存配置到文件中　*提示,原先配置文件中的注释信息将不会保存.
  * @param config 配置对象指针
  * @param filename 保存到的文件
  * @return 成功为１,失败为0
  */
-int ini_config_save(INI_CONFIG* config,const char* filename);
+int
+ini_config_save(INI_CONFIG *config, const char *filename);
 /**
  * 类似于ini_config_save,只是参数是文件指针,此函数可以直接使用stdin,stdout,stderr.　*提示:本函数不负责关闭fp.
  * @param config 配置对象指针
  * @param fp 文件指针
  * @return 成功为１,失败为0
  */
-int ini_config_print(INI_CONFIG* config,FILE* fp);
+int
+ini_config_print(INI_CONFIG *config, FILE *fp);
 
 #endif
