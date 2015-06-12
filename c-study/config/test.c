f7d4d5e184f46a82e98066043b8ef704aef8d22b
Jerry Yang <hy0kle@gmail.com>
2015-06-12 11:57:35 +0800

    新增c语言解析 ini 配置文件功能.

---
+++ c-study/config/test.c
@@ -0,0 +1,61 @@
+/**
+ * @describe:
+ * @author: Jerry Yang(hy0kle@gmail.com)
+ * */
+// gcc -o test confile.c test.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <time.h>
+#include <assert.h>
+#include <signal.h>
+#include <errno.h>
+#include <err.h>
+#include <getopt.h>
+#include <sys/time.h>
+#include <sys/queue.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+
+#include "confile.h"
+ 
+int main(int argc, char* argv[])
+{
+    INI_CONFIG *config;
+
+    printf("------------test1-----------\n");
+    config = ini_config_create_from_string("hover     =      lees       \n data =    7 \n maxthread=255\n;this is a comment line\r\nyahoo    =alibaba     \n     [section1]   \nhover   =  lees2  \nhover=lees333\n\n hover = lees4444\nyahoo=3\n\n\n", 0, 0);
+
+    if (config) {
+        ini_config_print(config, stdout);
+        ini_config_destroy(config);
+    }
+ 
+    printf("\n------------test2-----------\n");
+    config = ini_config_create_from_file("php.ini", 0);
+    if (config) {
+        printf(
+            "%s %s\n",
+            ini_config_get_string(config, "soap", "soap.wsdl_cache_dir", "/nodir"),
+            ini_config_get_string(config, "soap", "soap.wsdl_cache_ttl", "xixi")
+        );
+        ini_config_destroy(config);
+    } else {
+        err(1, "Can NOT init cofing from file");
+    }
+
+    return 0;
+}
+/* vi:set ft=c ts=4 sw=4 et fdm=marker: */
+
