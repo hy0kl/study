984b4e3a5144d23e2768801f00e1089f48f5bfb7
Jerry Yang <hy0kle@gmail.com>
2015-10-13 12:33:38 +0800

    完善程序.

--- c-study/popen.c
+++ c-study/popen.c
@@ -32,20 +32,42 @@
 
 int main(int argc, char *argv[])
 {
+    int   rc = 0;
     FILE *fp;
-    char line_buf[LINE_BUF_LEN];
-    char output_buf[OUTPUT_BUF_LEN];
+    char  line_buf[LINE_BUF_LEN];
+    char  cmd_buf[LINE_BUF_LEN];
+    char  output_buf[OUTPUT_BUF_LEN];
 
     output_buf[0] = '\0';
-    fp = popen("cd ~/ && ls -la", "r");
+    if (argc > 1) {
+        snprintf(cmd_buf, LINE_BUF_LEN, "%s", argv[1]);
+    } else {
+        snprintf(cmd_buf, LINE_BUF_LEN, "cd ~/ && ls -la");
+    }
+    //fp = popen("cd ~/ && ls -la", "r");
+    //fp = popen("ls -l nofile.txt 2>&1", "r");
+    fp = popen(cmd_buf, "r");
+    if (NULL == fp) {
+        fprintf(stderr, "Can NOT popen().\n");
+        exit(110);
+    }
+
     while (strlen(output_buf) < sizeof(output_buf) - sizeof(line_buf) && NULL != fgets(line_buf, sizeof(line_buf) - 1, fp)) {
         strncat(output_buf, line_buf, strlen(line_buf));
     }
 
-    printf("子进程的执行结果: \n\n%s\n---end--\n", output_buf);
-    pclose(fp);
+    rc = pclose(fp);
+    int status_child = WEXITSTATUS(rc);
 
-    return 0;
+    if (0 == status_child) {
+        printf("子进程执行成功! <-> ");
+    } else {
+        printf("子进程执行失败! <-> ");
+    }
+
+    printf("子进程的执行结果: \n\n%s\n---end--\n", output_buf);
+
+    return EXIT_SUCCESS;
 }
 
 /* vi:set ft=c ts=4 sw=4 et fdm=marker: */
