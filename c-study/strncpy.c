8c4f46c63ef00754ca18df20e8c1f3741b2c770d
Jerry Yang <hy0kle@gmail.com>
2015-12-31 23:08:19 +0800

    strncpy 函数样例.

--- c-study/strncpy.c
+++ c-study/strncpy.c
@@ -8,15 +8,18 @@
 int main(int argc, char *argv[])
 {
     char str_buf[MAX_BUF_LEN] = "Just test strncpy function.";
+    char *p;
     
+    printf("str init: %s\n", str_buf);
+
     if (argc > 1 && 0 != argv[1][0])
     {
         //snprintf(url, MAX_URL_LEN, "%s", argv[1]);
-        strncpy(str_buf, argv[1], COPY_LEN);
+        p = strncpy(str_buf, argv[1], COPY_LEN);
     }
 
-    printf("%s\n", str_buf);
-    printf("str_buf.length = %d\n", (int)strlen(str_buf));
+    printf("str_buf.length = %d v: %s\n", (int)strlen(str_buf), str_buf);
+    printf("p: %s\n", p);
 
     return 0;
 }
