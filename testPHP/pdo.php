45ead9db6a8cfeab5c55fde2bc2f2bd1c4a1403b
Jerry Yang <hy0kle@gmail.com>
2016-05-12 14:40:42 +0800

    pdo 增加一些测试代码,以更深入理解pdo

--- testPHP/pdo.php
+++ testPHP/pdo.php
@@ -9,14 +9,28 @@ $password = 'test';
 
 try {
     $db = new PDO($dsn, $user, $password);
+    //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); //设置为警告模式
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //设置异常模式
 
-    $sql = 'SELECT * FROM log';
-    $res = $db->query($sql, PDO::FETCH_ASSOC);
+    $sql = 'SELECT * FROM log WHERE id = ? AND create_time > 0';
 
-    $rows = $res->fetchAll();
+    //$res = $db->query($sql, PDO::FETCH_ASSOC);
+    //$rows = $res->fetchAll();
+    //print_r($rows);
+
+    $stmt = $db->prepare($sql);   //准备查询语句
+    $id = 2;
+    $stmt->bindParam(1, $id);
+    $stmt->execute();
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     print_r($rows);
 } catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage() . PHP_EOL;
+    echo '[PDO Exception Caught]' . PHP_EOL;
+    echo 'ERROR: ' . $e->getMessage() . PHP_EOL;
+    echo 'Code:  ' . $e->getCode() . PHP_EOL;
+    echo 'File:  ' . $e->getFile() . PHP_EOL;
+    echo 'Line:  ' . $e->getLine() . PHP_EOL;
+    echo 'Trace: ' . $e->getTraceAsString() . PHP_EOL;
 }
 
 /* vi:set ts=4 sw=4 et fdm=marker: */
