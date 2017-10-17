8e192d5ea83d2c3bb7245d7092de7de43b23d293
Jerry Yang <hy0kle@gmail.com>
2017-10-17 19:15:27 +0800

    php自行捕获出错,保存现场

---
+++ testPHP/error.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @describe: 自行捕获出错,保存现场
+ * @author: Jerry Yang(hy0kle@gmail.com)
+ * */
+//set_error_handler('cus_error_handler');
+set_error_handler('cus_error_handler', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+
+function cus_error_handler($errno, $errstr, $errfile, $errline, $errcontext) {
+    print_r(sprintf("errno: %d | errstr: %s | errfile: %s | errline: %d | errcontext: %s\n", $errno, $errstr, $errfile, $errline, json_encode($errcontext)));
+}
+
+echo $abc;
+echo sprintf("Just 4 test %s %d\n");
+/* vim:set ts=4 sw=4 et fdm=marker: */
+
