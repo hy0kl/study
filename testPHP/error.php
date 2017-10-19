bc924774d165a2b83f913e72fb2b0032f435acb6
Jerry Yang <hy0kle@gmail.com>
2017-10-19 10:45:47 +0800

    some small improve

--- testPHP/error.php
+++ testPHP/error.php
@@ -7,7 +7,9 @@
 set_error_handler('cus_error_handler', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
 
 function cus_error_handler($errno, $errstr, $errfile, $errline, $errcontext) {
-    print_r(sprintf("errno: %d | errstr: %s | errfile: %s | errline: %d | errcontext: %s\n", $errno, $errstr, $errfile, $errline, json_encode($errcontext)));
+    print_r(sprintf("errno: %d | errstr: %s | errfile: %s | errline: %d | errcontext: %s | request_uri: %s\n",
+        $errno, $errstr, $errfile, $errline, json_encode($errcontext),
+        isset($_SERVER['REQUEST_URI']) ? json_encode($_SERVER['REQUEST_URI']) : 'cli'));
 }
 
 echo $abc;
